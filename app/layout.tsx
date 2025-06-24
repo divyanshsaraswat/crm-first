5405fe3d594f01a42c5b66b04356284afb888d6c
Divyansh Saraswat <divyanshsaraswatofficial@gmail.com>
2025-06-24 22:50:55 +0530

    Desc and Title Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ConvertCRM",
+  title: "ConvertCRM - Transform Leads IntoLoyal Customers",
   description: "Customer Relation Management App",
 };
 
